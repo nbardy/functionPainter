ee3b3bb12c9898b86367a844eb5122a85cf03234
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-02-18 14:52:15 -0500

    Beautified code

--- painter.js
+++ painter.js
@@ -1,28 +1,25 @@
 function update() {
+   //Retrieve information from the dom
+   canvasDraw = document.getElementById('painter');
+   context = canvasDraw.getContext('2d');
+   //Retrieve height and width
+   width = parseInt(canvasDraw.width);
+   height = parseInt(canvasDraw.height);
+
+   //Retrieve functions for drawing each color
    redstring = document.getElementById('redFunction').value;
    greenstring = document.getElementById('greenFunction').value;
    bluestring = document.getElementById('blueFunction').value;
 
-   canvas1 = document.getElementById('painter');
-   context = canvas1.getContext('2d');
-
-   width = parseInt(canvas1.width);
-   height = parseInt(canvas1.height);
-
-   imageData = context.createImageData(width, height);
-
-   redstring = document.getElementById('redFunction').value;
-   greenstring = document.getElementById('greenFunction').value;
-   bluestring = document.getElementById('blueFunction').value;
-
+   //Fill pixel grid with data
    drawPicture(imageData, width, height, redstring, greenstring, bluestring);
 
+   //Place picture on canvas
    context.putImageData(imageData, 0, 0);
 }
 
 function drawPicture(imageData, width, height, redstring, greenstring, bluestring) {
    pos = 0; // index position into imagedata array
-
 
    // walk left-to-right, top-to-bottom; it's the
    // same as the ordering in the imagedata array:
@@ -37,20 +34,12 @@ function drawPicture(imageData, width, height, redstring, greenstring, bluestrin
          g = eval(greenstring);
 
          // set red, green, blue, and alpha:
-         imageData.data[pos++] = Math.max(0,Math.min(255, r));
-         imageData.data[pos++] = Math.max(0,Math.min(255, g));
-         imageData.data[pos++] = Math.max(0,Math.min(255, b));
+         imageData.data[pos++] = clamp(r);
+         imageData.data[pos++] = clamp(g);
+         imageData.data[pos++] = clamp(b);
          imageData.data[pos++] = 0xff; // alpha
      }
    }
-}
-
-function setPixel(imageData, x, y, r, g, b, a) {
-   index = (x + y * imageData.width) * 4;
-   imageData.data[index+0] = r;
-   imageData.data[index+1] = g;
-   imageData.data[index+2] = b;
-   imageData.data[index+3] = a;
 }
 
 function clamp(value) {
